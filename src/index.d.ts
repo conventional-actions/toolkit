c00d5e960b2aba643b74de7b29f50cf5daa58e12
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-08-29 15:19:11 -0700

    fix: tsconfig

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@conventional-actions/toolkit" {
-  export function parseMultiInput(files: string): string[]
-  export function paths(patterns: string[]): string[]
-  export function unmatchedPatterns(patterns: string[]): string[]
-}
